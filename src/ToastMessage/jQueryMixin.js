a90a47ceb6f4365927d6ba42acd4570b9a928c9b
tomchentw <developer@tomchentw.com>
2014-10-13 01:18:40 +0800

    feat(jQueryMixin): add show props and hook to componentDidMount

--- src/ToastMessage/jQueryMixin.js
+++ src/ToastMessage/jQueryMixin.js
@@ -3,7 +3,38 @@ module.exports = {
     return {
       style: {
         display: "none" // effective $.hide()
-      }
+      },
+      showMethod: "fadeIn", //, slideDown, and show are built into jQuery
+      showDuration: 300,
+      showEasing: "swing", // and linear are built into jQuery
+      hideMethod: "fadeOut",
+      hideDuration: 1000,
+      hideEasing: "swing",
+      //
+      timeOut: 5000,
     };
+  },
+
+  getInitialState () {
+    return {
+      intervalId: null
+    };
+  },
+
+  _get$Node () {
+    return $(this.getDOMNode());
+  },
+
+  componentDidMount () {
+    var {props} = this;
+    this._get$Node()[props.showMethod]({
+      duration: props.showDuration,
+      easing: props.showEasing
+    });
+    // if (props.timeOut > 0) {
+    //   this.setState({
+    //     intervalId: setTimeout('hideToast', props.timeOut);
+    //   });
+    // }
   }
 };
