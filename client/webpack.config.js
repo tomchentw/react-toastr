ad0f56af6017a969bc2c6e220e9d74821779e32a
tomchentw <developer@tomchentw.com>
2014-10-15 22:12:55 +0800

    chore(package.json): move to client/webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,15 +1,15 @@
-var Path = require('path');
-var webpack = require('webpack');
+var Path = require("path");
+var webpack = require("webpack");
 
 var JSX_LOADER = "jsx-loader?harmony";
 var SCSS_LOADER = "style-loader!css-loader!sass-loader?includePaths[]=" +
-        Path.resolve(__dirname, './bower_components/bootstrap-sass-official/assets/stylesheets');
-var IS_PRODUCTION = 'production' === process.env.NODE_ENV;
+        Path.resolve(__dirname, "../bower_components/bootstrap-sass-official/assets/stylesheets");
+var IS_PRODUCTION = "production" === process.env.NODE_ENV;
 
 var webpackConfig = module.exports = {
   entry: "./client/scripts/index.js",
   output: {
-    path: "./public",
+    path: "../public",
     filename: (IS_PRODUCTION ? "assets/[hash].js" : "assets/bundle.js")
   },
   module: {
