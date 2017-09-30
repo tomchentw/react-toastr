c832e92ffe1043e526c932865944d8badf5589bd
Tom Chen <developer@tomchentw.com>
2017-09-30 13:16:05 +0800

    chore(styleguide.config.js): add

---
+++ styleguide.config.js
@@ -0,0 +1,14 @@
+const _ = require("lodash")
+const path = require("path")
+
+module.exports = {
+  require: ["animate.css", "toastr/build/toastr.min.css"],
+  showUsage: true,
+  getComponentPathLine(componentPath) {
+    const name = path.basename(componentPath, ".jsx")
+    if (name === "ToastMessagejQuery") {
+      return `import ${name} from "react-toastr/lib/components/ToastMessage/ToastMessagejQuery";`
+    }
+    return `import { ${name} } from "react-toastr";`
+  },
+}
