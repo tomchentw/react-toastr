4194a41cbdebd7ca456b91c5aac9c65fe9e26fc5
Param Choudhary <69947192+Paramchoudhary@users.noreply.github.com>
2021-08-27 17:05:32 +0530

    remove extra spaces from the code index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 export { ToastContainer } from "./components/ToastContainer"
-
 export { ToastMessage } from "./components/ToastMessage/ToastMessage"
-
 export {
   ToastMessageAnimated,
 } from "./components/ToastMessage/ToastMessageAnimated"
